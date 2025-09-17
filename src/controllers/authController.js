352d949e730e79d5bdf1a0f8181390de3174f6d7
Roland Bekono <bekonrolang@gmail.com>
2025-09-17 14:49:31 +0100

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,7 +24,7 @@ exports.register = async (req, res, next) => {
     }
     const user = await User.create({
       name, email, ageRange,
-      gender, phone, password: hash, role, whatsapp_number, newLocation, contacts_count, audience
+      gender, phone, password: hash, role, whatsapp_number, location:newLocation, contacts_count, audience
     });
     
     // Logger l'activité selon le rôle
